9c2c33678bb4e579db5f7245dadb3a5d9363c296
Zakaria ElQotbi <zakaria@elqotbi.com>
2024-06-05 21:17:44 +0200

    enrich planning_utils with RRT and other utils

--- planning_utils.py
+++ planning_utils.py
@@ -1,11 +1,79 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
-
-import numpy.linalg as LA
 import networkx as nx
+import numpy.linalg as LA
+from sklearn.neighbors import KDTree
+from shapely.geometry import Polygon, Point, LineString
+
 from scipy.spatial import Voronoi
 from bresenham import bresenham
+
+class RRT:
+    def __init__(self, x_init):
+        # A tree is a special case of a graph with
+        # directed edges and only one path to any vertex.
+        self.tree = nx.DiGraph()
+        self.tree.add_node(x_init)
+
+    def add_vertex(self, x_new):
+        self.tree.add_node(tuple(x_init))
+
+    def add_edge(self, x_near, x_new, u):
+        self.tree.add_edge(tuple(x_near), tuple(x_new), orientation=u)
+
+    @property
+    def vertices(self):
+        return self.tree.nodes()
+
+    @property
+    def edges(self):
+        return self.tree.edges()
+
+
+def sample_state(grid):
+    maxx, maxy = grid.shape
+    while True:
+        x = np.random.randint(maxx)
+        y = np.random.randint(maxy)
+        if grid[x,y] == 0:
+            break
+
+    return (x, y)
+
+def nearest_neighbor(x_rand, rrt):
+    mylist = np.array([*rrt.vertices])
+    idx = np.linalg.norm(mylist - np.array(x_rand), axis=1).argmin()
+    return mylist[idx]
+
+def select_input(x_rand, x_near):
+    return np.arctan2(x_rand[1]-x_near[1], x_rand[0]-x_near[0])
+
+def new_state(x_near, u, dt):
+    x = x_near[0] + np.cos(u)*dt
+    y = x_near[1] + np.sin(u)*dt
+    return [x, y]
+
+def generate_RRT(grid, x_init, num_vertices, dt):
+    rrt = RRT(x_init)
+
+    for _ in range(num_vertices):
+
+        x_rand = sample_state(grid)
+        # sample states until a free state is found
+        while grid[int(x_rand[0]), int(x_rand[1])] == 1:
+            x_rand = sample_state(grid)
+
+        x_near = nearest_neighbor(x_rand, rrt)
+        u = select_input(x_rand, x_near)
+        x_new = new_state(x_near, u, dt)
+
+        if grid[int(x_new[0]), int(x_new[1])] == 0:
+            # the orientation `u` will be added as metadata to
+            # the edge
+            rrt.add_edge(x_near, x_new, u)
+
+    return rrt
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -183,6 +251,33 @@ def collinearity_check(p1, p2, p3, epsilon=1e-6):
     det = np.linalg.det(m)
     return abs(det) < epsilon
 
+def prune_path_sol(path):
+    pruned_path = [p for p in path]
+    # TODO: prune the path!
+
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+
+    print("prune path from {} to {} ".format(len(path), len(pruned_path)))
+
+    return pruned_path
+
 def collision_check(grid, p1, p2):
     cells = list(bresenham(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1])))
     hit = False
@@ -216,7 +311,7 @@ def prune_path_graph(path, grid):
 
         pruned_path.append(path[-1])
 
-    print("prune path from {} to {} ".format(len(path), len(pruned_path)))
+    print("\tPrune path from {} to {} ".format(len(path), len(pruned_path)))
 
     return pruned_path
 
@@ -257,7 +352,24 @@ def closest_point(graph, current_point):
             dist = d
     return closest_point
 
-def a_star_grid(grid, h, start, goal):
+def create_graph(nodes, k, polygons):
+    g = nx.Graph()
+    tree = KDTree(nodes)
+    for n1 in nodes:
+        # for each node connect try to connect to k nearest nodes
+        idxs = tree.query([n1], k, return_distance=False)[0]
+
+        for idx in idxs:
+            n2 = nodes[idx]
+            if n2 == n1:
+                continue
+
+            if can_connect(n1, n2, polygons):
+                g.add_edge(n1, n2, weight=1)
+    return g
+
+def a_star_graph_sol(graph, h, start, goal):
+    """Modified A* to work with NetworkX graphs."""
 
     path = []
     path_cost = 0
@@ -281,16 +393,14 @@ def a_star_grid(grid, h, start, goal):
             found = True
             break
         else:
-            for action in valid_actions(grid, current_node):
-                # get the tuple representation
-                da = action.delta
-                next_node = (current_node[0] + da[0], current_node[1] + da[1])
-                branch_cost = current_cost + action.cost
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node, next_node]['weight']
+                branch_cost = current_cost + cost
                 queue_cost = branch_cost + h(next_node, goal)
 
                 if next_node not in visited:
                     visited.add(next_node)
-                    branch[next_node] = (branch_cost, current_node, action)
+                    branch[next_node] = (branch_cost, current_node)
                     queue.put((queue_cost, next_node))
 
     if found:
@@ -352,6 +462,59 @@ def a_star_graph(graph, heuristic, start, goal):
         path.append(branch[n][1])
 
     return path[::-1], path_cost
+
+
+def a_star_grid(grid, h, start, goal):
+
+    path = []
+    path_cost = 0
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+
+    while not queue.empty():
+        item = queue.get()
+        current_node = item[1]
+        if current_node == start:
+            current_cost = 0.0
+        else:
+            current_cost = branch[current_node][0]
+
+        if current_node == goal:
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for action in valid_actions(grid, current_node):
+                # get the tuple representation
+                da = action.delta
+                next_node = (current_node[0] + da[0], current_node[1] + da[1])
+                branch_cost = current_cost + action.cost
+                queue_cost = branch_cost + h(next_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    branch[next_node] = (branch_cost, current_node, action)
+                    queue.put((queue_cost, next_node))
+
+    if found:
+        # retrace steps
+        n = goal
+        path_cost = branch[n][0]
+        path.append(goal)
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1])
+    else:
+        print('**********************')
+        print('Failed to find a path!')
+        print('**********************')
+    return path[::-1], path_cost
+
 
 
 def heuristic(position, goal_position):
